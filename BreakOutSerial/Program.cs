dc811764a5db3cb23bf078a20a7d72933351204e
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-09-05 08:50:15 +1200

    Updated Nugets and killed of code which is not required.

--- BreakOutSerial/Program.cs
+++ BreakOutSerial/Program.cs
@@ -14,79 +14,34 @@
 // limitations under the License.
 //
 // https://docs.rakwireless.com/Product-Categories/WisDuo/RAK4200-Breakout-Board/AT-Command-Manual/
+//
+// // nanoff --update --target ESP32_PSRAM_REV0 --serialport COM4
 //---------------------------------------------------------------------------------
-#define SERIAL_ASYNC_READ
-//#define SERIAL_THREADED_READ
-#define ST_STM32F769I_DISCOVERY      // nanoff --target ST_STM32F769I_DISCOVERY --update 
-//#define SPARKFUN_ESP32_THING_PLUS   // nanoff --platform esp32 --serialport COM4 --update
-//#define RAK_WISBLOCK_RAK2305 // nanoff --update --target ESP32_PSRAM_REV0 --serialport COM4
-// May 2022 Still experiencing issues with ComPort assignments
-//#define NETDUINO3_WIFI   // nanoff --target NETDUINO3_WIFI --update
-//#define ST_NUCLEO64_F091RC // nanoff --target ST_NUCLEO64_F091RC --update 
-//#define ST_NUCLEO144_F746ZG //nanoff --target ST_NUCLEO144_F746ZG --update
-
 namespace devMobile.IoT.LoRaWAN.nanoFramework.RAK4200
 {
 	using System;
 	using System.Diagnostics;
 	using System.IO.Ports;
 	using System.Threading;
-#if SPARKFUN_ESP32_THING_PLUS || RAK_WISBLOCK_RAK2305
+
    using global::nanoFramework.Hardware.Esp32; //need NuGet nanoFramework.Hardware.Esp32
-#endif
 
    public class Program
 	{
 		private static SerialPort _SerialPort;
-#if SERIAL_THREADED_READ
-		private static Boolean _Continue = true;
-#endif
-#if SPARKFUN_ESP32_THING_PLUS
-		private const string SerialPortId = "COM2";
-#endif
-#if RAK_WISBLOCK_RAK2305
       private const string SerialPortId = "COM2";
-#endif
-#if NETDUINO3_WIFI
-      private const string SerialPortId = "COM3";
-#endif
-#if MBN_QUAIL
-      private const string SpiBusId = "";
-#endif
-#if ST_NUCLEO64_F091RC
-      private const string SerialPortId = "";
-#endif
-#if ST_NUCLEO144_F746ZG
-      private const string SerialPortId = "";
-#endif
-#if ST_STM32F429I_DISCOVERY
-      private const string SerialPortId = "";
-#endif
-#if ST_STM32F769I_DISCOVERY
-		private const string SerialPortId = "COM6";
-#endif
 
       public static void Main()
 		{
 			Thread.Sleep(5000);
-
-#if SERIAL_THREADED_READ
-			Thread readThread = new Thread(SerialPortProcessor);
-#endif
 
 			Debug.WriteLine("devMobile.IoT.LoRaWAN.nanoFramework.RAK4200 BreakoutSerial starting");
 
 			try
 			{
             // set GPIO functions for COM2 (this is UART1 on ESP32)
-#if SPARKFUN_ESP32_THING_PLUS
-				Configuration.SetPinFunction(Gpio.IO17, DeviceFunction.COM2_TX);
-				Configuration.SetPinFunction(Gpio.IO16, DeviceFunction.COM2_RX);
-#endif
-#if RAK_WISBLOCK_RAK2305
             Configuration.SetPinFunction(Gpio.IO21, DeviceFunction.COM2_TX);
 				Configuration.SetPinFunction(Gpio.IO19, DeviceFunction.COM2_RX);
-#endif
 
             Debug.Write("Ports:");
 				foreach (string port in SerialPort.GetPortNames())
@@ -98,8 +53,8 @@ namespace devMobile.IoT.LoRaWAN.nanoFramework.RAK4200
 				using (_SerialPort = new SerialPort(SerialPortId))
 				{
 					// set parameters
-					_SerialPort.BaudRate = 9600;
-					//_SerialPort.BaudRate = 115200;
+					//_SerialPort.BaudRate = 9600;
+					_SerialPort.BaudRate = 115200;
 					_SerialPort.Parity = Parity.None;
 					_SerialPort.DataBits = 8;
 					_SerialPort.StopBits = StopBits.One;
@@ -114,17 +69,11 @@ namespace devMobile.IoT.LoRaWAN.nanoFramework.RAK4200
 
 					//_SerialPort.WatchChar = '\n'; // May 2022 WatchChar event didn't fire github issue https://github.com/nanoframework/Home/issues/1035
 
-#if SERIAL_ASYNC_READ
 					_SerialPort.DataReceived += SerialDevice_DataReceived;
-#endif
 
 					_SerialPort.Open();
 
 					_SerialPort.WatchChar = '\n';
-
-#if SERIAL_THREADED_READ
-					readThread.Start();
-#endif
 
 					for (int i = 0; i < 5; i++)
 					{
@@ -153,7 +102,6 @@ namespace devMobile.IoT.LoRaWAN.nanoFramework.RAK4200
 			}
 		}
 
-#if SERIAL_ASYNC_READ
 		private static void SerialDevice_DataReceived(object sender, SerialDataReceivedEventArgs e)
 		{
 			SerialPort serialPort = (SerialPort)sender;
@@ -172,26 +120,5 @@ namespace devMobile.IoT.LoRaWAN.nanoFramework.RAK4200
 					break;
 			}
 		}
-#endif
-
-#if SERIAL_THREADED_READ
-		public static void SerialPortProcessor()
-		{
-
-			while (_Continue)
-			{
-				try
-				{
-					string response = _SerialPort.ReadLine();
-					//string response = _SerialPort.ReadExisting();
-					Debug.Write(response);
-				}
-				catch (TimeoutException ex) 
-				{
-					Debug.WriteLine($"Timeout:{ex.Message}");
-				}
-			}
-		}
-#endif
 	}
 }
